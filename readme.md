6c781f8cfad291518158aca2cfe4dbdf6d5e289c
Cristian Pallarés <cristianp@wing.cloud>
2025-02-06 22:13:33 +0100

    chore: initial commit

---
+++ readme.md
@@ -0,0 +1,48 @@
+# devalue-codec
+
+Uses [devalue](https://github.com/Rich-Harris/devalue) to serialize expressions while retaining the original object type.
+
+Accepts the following types, including arrays and objects:
+
+- `string`
+- `number`
+- `boolean`
+- `Date`
+- `bigint`
+- `Map`
+- `Set`
+- `ArrayBuffer`
+- `Int8Array`
+- `Int16Array`
+- `Int32Array`
+- `Uint8Array`
+- `Uint16Array`
+- `Uint32Array`
+- `Uint8ClampedArray`
+- `RegExp`
+- `undefined`
+- `null`
+
+## Installation
+
+```sh
+npm install devalue-codec
+```
+
+## Usage
+
+```ts
+import * as codec from "devalue-codec";
+
+const encoded = codec.stringify({
+	key: "value",
+});
+
+const decoded = codec.parse(encoded);
+```
+
+## Other codecs
+
+- [@skyrpex/string-codec](https://github.com/skyrpex/string-codec)
+- [superjson-codec](https://github.com/skyrpex/superjson-codec)
+- [json-codec](https://github.com/skyrpex/json-codec)
---
+++ src/devalue-codec.test.ts
@@ -0,0 +1,77 @@
+import * as devalue from "devalue";
+import { expect, test } from "vitest";
+import * as codec from "./devalue-codec.ts";
+
+test("stringifies correctly using superjson", () => {
+	expect(
+		codec.stringify({
+			key: "value",
+		}),
+	).toEqual(
+		devalue.stringify({
+			key: "value",
+		}),
+	);
+});
+
+test("parses correctly", () => {
+	expect(
+		codec.parse(
+			codec.stringify({
+				key: "value",
+			}),
+		),
+	).toEqual({
+		key: "value",
+	});
+});
+
+test("works with dates", () => {
+	expect(codec.parse(codec.stringify(new Date("2025-01-01T01:01:01")))).toEqual(
+		new Date("2025-01-01T01:01:01"),
+	);
+});
+
+test("works with bigints", () => {
+	expect(codec.parse(codec.stringify(7777n))).toEqual(7777n);
+});
+
+test("works with maps", () => {
+	expect(codec.parse(codec.stringify(new Map([["key", "value"]])))).toEqual(
+		new Map([["key", "value"]]),
+	);
+});
+
+test("works with sets", () => {
+	expect(codec.parse(codec.stringify(new Set([1, 2, 3])))).toEqual(
+		new Set([1, 2, 3]),
+	);
+});
+
+for (const ArrayContrustor of [
+	Int8Array,
+	Int16Array,
+	Int32Array,
+	Uint8Array,
+	Uint16Array,
+	Uint32Array,
+	Uint8ClampedArray,
+]) {
+	test(`works with ${ArrayContrustor.name}`, () => {
+		expect(
+			codec.parse(codec.stringify(new ArrayContrustor([1, 2, 3]))),
+		).toEqual(new ArrayContrustor([1, 2, 3]));
+	});
+}
+
+test("works with undefined", () => {
+	expect(codec.parse(codec.stringify(undefined))).toBe(undefined);
+});
+
+test("works with null", () => {
+	expect(codec.parse(codec.stringify(null))).toBe(null);
+});
+
+test("works with regexp", () => {
+	expect(codec.parse(codec.stringify(/test/))).toEqual(/test/);
+});
