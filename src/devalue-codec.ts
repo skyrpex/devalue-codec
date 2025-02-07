bb6eebd14e15f52bfbaea9b211d7023cfb5be85a
Cristian Pallarés <cristianp@wing.cloud>
2025-02-08 00:28:52 +0100

    feat: adds check type

--- src/devalue-codec.ts
+++ src/devalue-codec.ts
@@ -1,16 +1,16 @@
-import { type SerializableRecord, parser, stringifier } from "codec-builder";
+import * as codecBuilder from "codec-builder";
 import * as devalue from "devalue";
 
 export type { Stringified } from "codec-builder";
 
-export type SerializablePrimitive =
+export type Allowed =
 	| string
 	| number
 	| boolean
 	| Date
 	| bigint
-	| Map<unknown, SerializablePrimitive>
-	| Set<SerializablePrimitive>
+	| Map<unknown, Allowed>
+	| Set<Allowed>
 	| Int8Array
 	| Int16Array
 	| Int32Array
@@ -22,21 +22,31 @@ export type SerializablePrimitive =
 	| undefined
 	| null;
 
-export type Serializable = SerializableRecord<SerializablePrimitive>;
+/**
+ * @deprecated Use {@link Check} instead.
+ */
+export type Serializable = codecBuilder.SerializableRecord<Allowed>;
 
 // biome-ignore lint/complexity/noBannedTypes: we need to forbid Function
 export type Forbidden = symbol | URL | Function;
 
 /**
- * Serializes a {@link SerializablePrimitive} object to a string.
+ * Checks if a value is serializable.
+ *
+ * @returns `T` if the value is serializable, `never` otherwise.
+ */
+export type Check<T> = codecBuilder.Serializable<T, Allowed, Forbidden>;
+
+/**
+ * Serializes a {@link Allowed} object to a string.
  */
 // @__NO_SIDE_EFFECTS__
-export const stringify = stringifier<SerializablePrimitive, Forbidden>(
+export const stringify = codecBuilder.stringifier<Allowed, Forbidden>(
 	devalue.stringify,
 );
 
 /**
- * Parses a stringified {@link SerializablePrimitive} object to its original object.
+ * Parses a stringified {@link Allowed} object to its original object.
  */
 // @__NO_SIDE_EFFECTS__
-export const parse = parser<SerializablePrimitive>(devalue.parse);
+export const parse = codecBuilder.parser<Allowed>(devalue.parse);
